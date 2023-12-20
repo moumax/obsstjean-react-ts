5c07644c5e7bb0b2c9cc0a328c42abf502f596f1
Marc LANTOL <marc.lantol@gmail.com>
2023-12-20 22:48:37 +0100

    es6 conversion and js transformation

---
+++ backend/src/helpers/jwtHelper.js
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken';
+
+export const encodeJwt = (payload) => {
+  return jwt.sign(payload, process.env.TOKEN_SECRET, {
+    expiresIn: '7d',
+  });
+}
+
+export const decodeJwt = (token) => {
+  return jwt.verify(token, process.env.TOKEN_SECRET);
+}
