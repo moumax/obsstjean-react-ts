aaef7f2da409adafb7c1547580e4e580d16723b2
Marc LANTOL <marc.lantol@gmail.com>
2023-12-20 22:50:47 +0100

    es6 conversion

--- backend/src/services/db.js
+++ backend/src/services/db.js
@@ -1,6 +1,10 @@
-require("dotenv").config();
-const mysql = require("mysql2/promise");
+import dotenv from "dotenv";
+import mysql from "mysql2/promise";
+
+dotenv.config();
+
 const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
 
 const dbPool = mysql.createPool({
   host: DB_HOST,
@@ -8,22 +12,22 @@ const dbPool = mysql.createPool({
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
-})
+});
 
 dbPool.getConnection((err) => {
   if (err) {
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      console.error('Database connection was closed.');
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error("Database connection was closed.");
     }
-    if (err.code === 'ER_CON_COUNT_ERROR') {
-      console.error('Database has too many connections.');
+    if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error("Database has too many connections.");
     }
-    if (err.code === 'ECONNREFUSED') {
-      console.error('Database connection was refused.');
+    if (err.code === "ECONNREFUSED") {
+      console.error("Database connection was refused.");
     }
-    console.error('Error creating MySQL pool:', err);
+    console.error("Error creating MySQL pool:", err);
     return;
   }
-})
+});
 
-module.exports = dbPool;
+export default dbPool;
