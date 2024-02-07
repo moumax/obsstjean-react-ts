f1d82a8d7fed8ed6b3c9d165e937156779703ac1
Marc LANTOL <marc.lantol@gmail.com>
2024-02-07 19:53:28 +0100

    Delete unnecessary code

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -2,25 +2,17 @@ import { decodeJwt } from "../helpers/jwtHelper.js";
 
 const authorization = (req, res, next) => {
   const token = req.cookies.access_token;
-  const toktok = req.cookies;
-  console.log("Toktok", toktok);
-
-  console.log("Token", token);
   if (!token) {
     return res.sendStatus(401);
   }
 
   try {
     const data = decodeJwt(token);
-
-    console.log("decoded data", data);
-
     req.userName = data.name;
     req.userRole = data.role;
     return next();
   } catch (error) {
     console.error("error decoding token", error);
-
     return res.send(401);
   }
 };
