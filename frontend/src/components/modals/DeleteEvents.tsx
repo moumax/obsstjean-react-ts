2f2780d814f160db32ed4ef1984bb4ad0e0d5f3d
Marc LANTOL <marc.lantol@gmail.com>
2024-02-08 15:45:42 +0100

    Fix: Change route link

--- frontend/src/components/modals/DeleteEvents.tsx
+++ frontend/src/components/modals/DeleteEvents.tsx
@@ -1,44 +1,64 @@
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog.tsx"
-import { Trash2 } from "lucide-react"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog.tsx";
+import { Trash2 } from "lucide-react";
 import { mutate } from "swr";
 import { useToast } from "../ui/use-toast";
 
-
-
 function DeleteEvents(props) {
-  const {toast} = useToast()
+  const { toast } = useToast();
 
   const handleDelete = async (eventId: number) => {
     try {
-      await fetch(`${import.meta.env.VITE_BACKEND_URL}/events/${eventId}`, {
+      await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/events/${eventId}`, {
         method: "DELETE",
       });
       toast({
         description: `L'évènement ${props.title} a bien été supprimé`,
-      })
-      mutate(`${import.meta.env.VITE_BACKEND_URL}/events/`);
+      });
+      mutate(`${import.meta.env.VITE_BACKEND_URL}/api/events/`);
     } catch (error) {
-      console.error("Erreur lors de la suppression de l'évènement", error)
+      console.error("Erreur lors de la suppression de l'évènement", error);
     }
-  }
+  };
 
   return (
     <AlertDialog>
-      <AlertDialogTrigger className="text-red-600"><Trash2 /></AlertDialogTrigger>
+      <AlertDialogTrigger className="text-red-600">
+        <Trash2 />
+      </AlertDialogTrigger>
       <AlertDialogContent className="bg-blue-800 text-white">
         <AlertDialogHeader>
-          <AlertDialogTitle>Etes vous certain de vouloir supprimer l'évènement {props.title} ?`</AlertDialogTitle>
+          <AlertDialogTitle>
+            Etes vous certain de vouloir supprimer l'évènement {props.title} ?`
+          </AlertDialogTitle>
           <AlertDialogDescription className="text-white-800">
-            Cette action est irréversible. Elle supprimera définitivement l'évènement.
+            Cette action est irréversible. Elle supprimera définitivement
+            l'évènement.
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
-          <AlertDialogCancel className="bg-green-600">Annuler</AlertDialogCancel>
-          <AlertDialogAction onClick={() => handleDelete(props.id)} className="bg-red-600">Supprimer</AlertDialogAction>
+          <AlertDialogCancel className="bg-green-600">
+            Annuler
+          </AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => handleDelete(props.id)}
+            className="bg-red-600"
+          >
+            Supprimer
+          </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
-  )
+  );
 }
 
-export default DeleteEvents
+export default DeleteEvents;
