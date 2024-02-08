13a8a65b063e8d901d2013d6bed420ee5a54a82d
Marc LANTOL <marc.lantol@gmail.com>
2024-02-08 21:51:56 +0100

    Add: Loader for sun section and loader component
    - Loader in sun section because of weight of the images
    - Loader component doesn't exist in shadcnUI

--- frontend/src/components/Sun.tsx
+++ frontend/src/components/Sun.tsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
+import { LoadingSpinner } from "@/components/ui/loader";
+
 function Sun() {
+  const [isLoading, setIsLoading] = useState([true, true]);
+
+  const handleImageLoad = (index: number) => {
+    setIsLoading((prev) => {
+      const newState = [...prev];
+      newState[index] = false;
+      return newState;
+    });
+  };
+
   return (
     <div>
-      <img
-        className="rounded-2xl"
-        src="https://www.spaceweatherlive.com/images/SDO/SDO_HMIIF_1024.jpg"
-      />
-      <img
-        className="rounded-xl"
-        src="https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0304.jpg"
-      />
+      {isLoading.map((loading, index: number) => (
+        <div className="flex justify-center" key={index}>
+          {loading && <LoadingSpinner size={72} />}
+          <img
+            className="rounded-2xl"
+            src={
+              index === 0
+                ? "https://www.spaceweatherlive.com/images/SDO/SDO_HMIIF_1024.jpg"
+                : "https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0304.jpg"
+            }
+            onLoad={() => handleImageLoad(index)}
+            style={{ display: loading ? "none" : "block" }}
+          />
+        </div>
+      ))}
     </div>
   );
 }
