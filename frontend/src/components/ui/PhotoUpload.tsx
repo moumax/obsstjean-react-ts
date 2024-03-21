3f98a9a4c9b90b51cb1922a6670ef41743c25b9c
Marc LANTOL <marc.lantol@gmail.com>
2024-03-21 15:40:54 +0100

    Add: File size & file type
    - File size is now limited
    - File type is now limited

--- frontend/src/components/ui/PhotoUpload.tsx
+++ frontend/src/components/ui/PhotoUpload.tsx
@@ -4,18 +4,25 @@ import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orien
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
 import FilePondPluginImageTransform from 'filepond-plugin-image-transform'
 import FilePondPluginImageResize from 'filepond-plugin-image-resize'
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type'
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size'
 import './filepond.css'
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
 import { useRef, useState } from 'react'
 import { Label } from './label'
 import { Input } from './input'
 import { useAuth } from '@/contexts/AuthContext'
+import { Button } from '@/components/ui/button'
+import { mutate } from 'swr'
+import { toast } from 'sonner'
 
 registerPlugin(
   FilePondPluginImageExifOrientation,
   FilePondPluginImagePreview,
   FilePondPluginImageResize,
-  FilePondPluginImageTransform
+  FilePondPluginImageTransform,
+  FilePondPluginFileValidateType,
+  FilePondPluginFileValidateSize
 )
 
 function PhotoUpload() {
@@ -28,14 +35,24 @@ function PhotoUpload() {
   const { userName } = useAuth()
 
   const handleUpload = () => {
-    if (filePondRef.current) {
+    if (filePondRef.current !== null && image.length > 0) {
       filePondRef.current.processFiles()
+    } else {
+      console.error('Aucun fichier sélectionné.')
     }
   }
 
   const handleFileChange = files => {
     setImage(files)
     setFilesSelected(files.length > 0)
+  }
+
+  const handleProcessFile = () => {
+    setTitle('')
+    setDescription('')
+    filePondRef.current.removeFiles()
+    mutate(`${import.meta.env.VITE_BACKEND_URL}/api/gallery/${userName}`)
+    toast.success('Ta photo est sur le serveur !')
   }
 
   return (
@@ -50,7 +67,7 @@ function PhotoUpload() {
           onChange={e => {
             setTitle(e.target.value)
           }}
-          placeholder='Titre de ton image'
+          placeholder='Titre obligatoire'
         />
         <Label>Description de la photo</Label>
         <Input
@@ -60,47 +77,63 @@ function PhotoUpload() {
           onChange={e => {
             setDescription(e.target.value)
           }}
-          placeholder='Description de ton image'
+          placeholder='Description obligatoire'
         />
       </div>
-      <FilePond
-        labelIdle={`
-          <div style="width:100%;height:100%;">
+      <div className='flex flex-col border border-yellow-400 rounded-md mt-12 mb-12'>
+        <FilePond
+          labelIdle={`
+          <div style="width:100%;height:100%">
             <p>
               Fait glisser ton image ou <span class="filepond--label-action" tabindex="0">cherche sur ton disque dur</span><br>
             </p>
           </div>
         `}
-        files={image}
-        credits={false}
-        maxFiles={1}
-        instantUpload={false}
-        name='image'
-        allowReorder
-        imageTransformVariantsIncludeOriginal
-        allowImageResize
-        allowImageTransform
-        imageResizeTargetWidth={256}
-        server={{
-          process: {
-            url: baseURL,
-            method: 'POST',
-            withCredentials: true,
-            ondata: formData => {
-              formData.append('title', title)
-              formData.append('description', description)
-              formData.append('owner', userName)
-              return formData
+          labelFileProcessingComplete='Buennnoo'
+          labelFileProcessing='On bourre le fichier sur le serveur'
+          labelMaxFileSizeExceeded='Le fichier est trop balèze...'
+          labelMaxFileSize='Le poids du fichier dépasse {filesize}'
+          files={image}
+          ref={filePondRef}
+          credits={false}
+          maxFiles={1}
+          instantUpload={false}
+          name='image'
+          allowReorder
+          imageTransformVariantsIncludeOriginal
+          allowImageResize
+          allowImageTransform
+          allowFileTypeValidation
+          acceptedFileTypes={['image/jpg', 'image/jpeg']}
+          allowFileSizeValidation
+          maxFileSize='10MB'
+          imageResizeTargetWidth={256}
+          allowProcess={false}
+          server={{
+            process: {
+              url: baseURL,
+              method: 'POST',
+              withCredentials: true,
+              ondata: formData => {
+                formData.append('title', title)
+                formData.append('description', description)
+                formData.append('owner', userName)
+                return formData
+              }
             }
-          }
-        }}
-        onupdatefiles={handleFileChange}
-      />
-      {/* {filesSelected && ( // Affiche le bouton uniquement si des fichiers sont sélectionnés
-        <Button onClick={handleUpload} className="btn bg-green-500 mt-4 w-full mb-5">
-          Upload
-        </Button>
-      )} */}
+          }}
+          onupdatefiles={handleFileChange}
+          onprocessfile={handleProcessFile}
+        />
+        {filesSelected && title && description && (
+          <Button
+            onClick={handleUpload}
+            className=' bg-green-500 mt-4 w-[90%] mb-5 self-center'
+          >
+            Envoyer ta photo sur le serveur
+          </Button>
+        )}
+      </div>
     </div>
   )
 }
